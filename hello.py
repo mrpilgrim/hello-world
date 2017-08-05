020eb5b7fce42a72e0a802579b09cdc639dcc9fa
MrPilgrim <s.p.willis@gmail.com>
2017-08-05 15:23:02 +0100

    Initial python repo

---
+++ hello.py
@@ -0,0 +1,17 @@
+# import modules used here -- sys is a very usual one
+import sys
+
+# Gather our code in a main() function
+
+
+def main():
+    print("Hello there", sys.argv[1])
+    # Command line arguments are sys.argv[1], sys.argv[2]
+    # sys.argv[0] is the command itself and can be ignored
+
+# Standard boilerplate to call main() function to begin
+# the program
+
+
+if __name__ == '__main__':
+    main()
